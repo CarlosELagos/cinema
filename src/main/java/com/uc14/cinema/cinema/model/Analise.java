d544d82b81536c69a360e0eefe0326f63b15d77d
Carlos Lagos <carloseflagos@gmail.com>
2025-12-08 22:07:41 -0300

    criação das classes repository e service

--- src/main/java/com/uc14/cinema/cinema/model/Analise.java
+++ src/main/java/com/uc14/cinema/cinema/model/Analise.java
@@ -1,52 +1,70 @@
 package com.uc14.cinema.cinema.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Analise")
 public class Analise {
 
+    @Id //chave primaria
+    @GeneratedValue(strategy = GenerationType.AUTO) //auto incremento
     private int id;
-    private Filme filme;
     private String detalhes;
     private String nota;
 
-    public Analise() {
-    }
-
-    public Analise(int id, Filme filme, String detalhes, String nota) {
-        this.id = id;
-        this.filme = filme;
-        this.detalhes = detalhes;
-        this.nota = nota;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Filme getFilme() {
-        return filme;
-    }
-
-    public void setFilme(Filme filme) {
-        this.filme = filme;
-    }
-
-    public String getDetalhes() {
-        return detalhes;
-    }
-
-    public void setDetalhes(String detalhes) {
-        this.detalhes = detalhes;
-    }
-
-    public String getNota() {
-        return nota;
-    }
-
-    public void setNota(String nota) {
-        this.nota = nota;
-    }
+    @ManyToOne // Relacionamento n para 1
+    @JoinColumn(name="id_livro") //referencia para objeto filme
+    private Filme filme;
+    
+    
+//    public Analise() {
+//    }
+//
+//    public Analise(int id, Filme filme, String detalhes, String nota) {
+//        this.id = id;
+//        this.filme = filme;
+//        this.detalhes = detalhes;
+//        this.nota = nota;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public Filme getFilme() {
+//        return filme;
+//    }
+//
+//    public void setFilme(Filme filme) {
+//        this.filme = filme;
+//    }
+//
+//    public String getDetalhes() {
+//        return detalhes;
+//    }
+//
+//    public void setDetalhes(String detalhes) {
+//        this.detalhes = detalhes;
+//    }
+//
+//    public String getNota() {
+//        return nota;
+//    }
+//
+//    public void setNota(String nota) {
+//        this.nota = nota;
+//    }
 
 }
---
+++ src/main/java/com/uc14/cinema/cinema/repository/FilmeRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.uc14.cinema.cinema.repository;
+
+/**
+ *
+ * @author carlo
+ */
+public interface FilmeRepository {
+    
+}
